abf056ba9ecc4f2c9647b9e35cb78b883f257014
vladislav <fcm.ld@mail.ru>
2023-07-14 13:42:26 +0600

    hw2

---
+++ script2.js
@@ -0,0 +1,91 @@
+/*function CheckUser(user){
+    if (user === 'admin'){
+        return 'Welcome admin!'
+    } else{
+        return 'incorrect user'
+    }
+}
+
+console.log(CheckUser("admin"))*/
+
+/**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**/
+
+/*function Date(number){
+    if (number >=1 && number <=28){
+        return 'correct date!'
+} else{
+    return 'incorrect date'
+}
+}
+
+console.log(Date(12))*/
+
+/**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**/
+
+
+/*function ChekNumber(number){
+    if(number === 50){
+        return 'correct number!'
+    }else{
+        return 'incorrect number('
+    }
+}
+
+console.log(ChekNumber(50))*/
+
+
+/**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**/
+
+
+/*function CheckAge(age,user){
+    if (age >= 20 && age <= 60){
+        return `${user} ${age} years, welcome`
+    }else{
+        return `${user} ${age} not suitable!`
+    }
+}
+
+console.log(CheckAge('33' , 'vlad'))*/
+
+
+/**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**/
+/*
+function CheckName(user){
+    if( user.length <= 6){
+        return `${user} correct number!`
+    } else if (user.length <=3){
+        return 'to short number'
+    }
+    else  {
+        return `${user} incorrect number!`
+    }
+}
+
+console.log(CheckName('dg'))*/
+
+/**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**/
+
+function Typeoff(number){
+    if (number || number >=0){
+        if (typeof number === 'string'){
+            return `${number} must be a number`
+        }else{
+            return 'string'
+        }
+    }else{
+        return 'error'
+    }
+}
+
+console.log(Typeoff(20))
+
+/**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**/
+
+function StringNumber(number){
+
+}
+
+
+
+
+
