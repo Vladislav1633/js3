a34237b1a9b26afaa55880a59572508d6d7d779f
vladislav <fcm.ld@mail.ru>
2023-07-19 15:28:12 +0600

    hw 19 07

--- script2.js
+++ script2.js
@@ -1,89 +1,24 @@
-/*function CheckUser(user){
-    if (user === 'admin'){
-        return 'Welcome admin!'
-    } else{
-        return 'incorrect user'
+/*const array = (arg) =>{
+return arg.reduce((arg, el) => {
+    return arg + el
+    })
+}
+
+console.log(array(([1,2,3,4,5,6,7,8,9,10])))*/
+
+
+/**-*-*-*-*-*-*-*-*-*-*-*-*-**-*-*-*-*-*-*-*-*-**/
+
+
+/*const HandleSum = (arg) => {
+    let sum = 0;
+    for (let i = 0; i <= arg.length; i++){
+        sum += i
     }
+    return sum;
 }
 
-console.log(CheckUser("admin"))*/
-
-/**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**/
-
-/*function Date(number){
-    if (number >=1 && number <=28){
-        return 'correct date!'
-} else{
-    return 'incorrect date'
-}
-}
-
-console.log(Date(12))*/
-
-/**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**/
-
-
-/*function ChekNumber(number){
-    if(number === 50){
-        return 'correct number!'
-    }else{
-        return 'incorrect number('
-    }
-}
-
-console.log(ChekNumber(50))*/
-
-
-/**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**/
-
-
-/*function CheckAge(age,user){
-    if (age >= 20 && age <= 60){
-        return `${user} ${age} years, welcome`
-    }else{
-        return `${user} ${age} not suitable!`
-    }
-}
-
-console.log(CheckAge('33' , 'vlad'))*/
-
-
-/**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**/
-/*
-function CheckName(user){
-    if( user.length <= 6){
-        return `${user} correct number!`
-    } else if (user.length <=3){
-        return 'to short number'
-    }
-    else  {
-        return `${user} incorrect number!`
-    }
-}
-
-console.log(CheckName('dg'))*/
-
-/**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**/
-
-function Typeoff(number){
-    if (number || number >=0){
-        if (typeof number === 'string'){
-            return `${number} must be a number`
-        }else{
-            return 'string'
-        }
-    }else{
-        return 'error'
-    }
-}
-
-console.log(Typeoff(20))
-
-/**-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**/
-
-function StringNumber(number){
-
-}
+console.log(HandleSum([1,2,3,4,5]))*/
 
 
 
